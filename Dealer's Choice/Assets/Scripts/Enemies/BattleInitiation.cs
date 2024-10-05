6b3dff2686d6b55db6ceb1f6db98de34de8f4ad6
ZclipseCode <bmichaelbusiness@gmail.com>
2024-10-05 12:27:46 -0500

    face enemy

--- Dealer's Choice/Assets/Scripts/Enemies/BattleInitiation.cs
+++ Dealer's Choice/Assets/Scripts/Enemies/BattleInitiation.cs
@@ -14,13 +14,38 @@ public class BattleInitiation : MonoBehaviour
 
         if (!inBattle && Physics.Raycast(transform.position, transform.forward, out hit, maxDistance, playerLayer))
         {
-            PlayerMovement playerMovement = hit.transform.GetComponentInParent<PlayerMovement>();
+            Transform player = hit.transform;
+
+            PlayerMovement playerMovement = player.GetComponentInParent<PlayerMovement>();
             playerMovement.DisableInput();
 
-            PlayerCamera playerCamera = hit.transform.parent.GetComponentInChildren<PlayerCamera>();
+            PlayerCamera playerCamera = player.parent.GetComponentInChildren<PlayerCamera>();
             playerCamera.DisableInput();
 
             inBattle = true;
+
+            Camera cam = player.parent.GetComponentInChildren<Camera>();
+            StartCoroutine(FaceMe(cam));
+        }
+    }
+
+    IEnumerator FaceMe(Camera cam)
+    {
+        float lastRotationMagnitude = 360f;
+        float rotationSpeed = 1f;
+
+        while (Mathf.Abs(cam.transform.rotation.eulerAngles.magnitude - lastRotationMagnitude) > 0.001f)
+        {
+            Vector3 targetDirection = transform.position - cam.transform.position;
+            targetDirection = new Vector3(targetDirection.x, 0f, targetDirection.z);
+
+            Vector3 newDirection = Vector3.RotateTowards(cam.transform.forward, targetDirection, rotationSpeed * Time.deltaTime, 0.0f);
+
+            lastRotationMagnitude = cam.transform.rotation.eulerAngles.magnitude;
+
+            cam.transform.rotation = Quaternion.LookRotation(newDirection);
+
+            yield return null;
         }
     }
 
